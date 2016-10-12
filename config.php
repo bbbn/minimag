85182661691e19a74fe7f5559ffcaa56c67905ef
Антон Мосякин <anton.mosiakin@academmedia.com>
2016-10-12 11:59:14 +0700

    first commit

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:8888/PROJECT/mini/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:8888/PROJECT/mini/');
+
+// DIR
+define('DIR_APPLICATION', '/Applications/MAMP/htdocs/PROJECT/mini/catalog/');
+define('DIR_SYSTEM', '/Applications/MAMP/htdocs/PROJECT/mini/system/');
+define('DIR_IMAGE', '/Applications/MAMP/htdocs/PROJECT/mini/image/');
+define('DIR_LANGUAGE', '/Applications/MAMP/htdocs/PROJECT/mini/catalog/language/');
+define('DIR_TEMPLATE', '/Applications/MAMP/htdocs/PROJECT/mini/catalog/view/theme/');
+define('DIR_CONFIG', '/Applications/MAMP/htdocs/PROJECT/mini/system/config/');
+define('DIR_CACHE', '/Applications/MAMP/htdocs/PROJECT/mini/system/storage/cache/');
+define('DIR_DOWNLOAD', '/Applications/MAMP/htdocs/PROJECT/mini/system/storage/download/');
+define('DIR_LOGS', '/Applications/MAMP/htdocs/PROJECT/mini/system/storage/logs/');
+define('DIR_MODIFICATION', '/Applications/MAMP/htdocs/PROJECT/mini/system/storage/modification/');
+define('DIR_UPLOAD', '/Applications/MAMP/htdocs/PROJECT/mini/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'minimag');
+define('DB_PORT', '8889');
+define('DB_PREFIX', 'oc_');
